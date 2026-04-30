2808d3560c31f4e5882dddf9cd0d120a68eff181
Yuto Chau <yuto5@outlook.com>
2026-04-30 18:39:11 -0400

    Implement the operate function

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let n1;
+let op;
+let n2;
+
 function add(a, b) {
   return a + b;
 }
@@ -12,4 +16,16 @@ function multiply(a, b) {
 
 function divide(a, b) {
   return a / b;
+}
+
+function operate(op, n1, n2) {
+  if (op === "+") {
+    return add(n1, n2);
+  } else if (op === "-") {
+    return subtract(n1, n2);
+  } else if (op === "x") {
+    return multiply(n1, n2);
+  } else {
+    return divide(n1, n2);
+  }
 }
