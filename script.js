26354f0b56d85fa5a5c25b0ad1b84f746cc0bc74
Yuto Chau <yuto5@outlook.com>
2026-04-30 22:00:37 -0400

    Add event listeners to digit buttons and update state functions

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-let n1;
-let op;
-let n2;
+let n1 = 0;
+let op = null;
+let n2 = null;
+const digits = document.querySelectorAll(".digit");
+const display = document.querySelector(".display");
 
 function add(a, b) {
   return a + b;
@@ -21,11 +23,30 @@ function divide(a, b) {
 function operate(op, n1, n2) {
   if (op === "+") {
     return add(n1, n2);
-  } else if (op === "-") {
+  } else if (op === "−") {
     return subtract(n1, n2);
-  } else if (op === "x") {
+  } else if (op === "×") {
     return multiply(n1, n2);
   } else {
     return divide(n1, n2);
   }
+}
+
+function updateNumberVars(n) {
+  if (op === null) {
+    n1 = +(n1 + n);
+  } else {
+    n2 = n2 === null ? +n : +(n2 + n);
+  }
+}
+
+function updateDisplay() {
+  display.textContent = `${n1}${op !== null ? ` ${op}` : ""}${n2 !== null ? ` ${n2}` : ""}`;
+}
+
+for (const digit of digits) {
+  digit.addEventListener("click", () => {
+    updateNumberVars(digit.textContent);
+    updateDisplay();
+  });
 }
