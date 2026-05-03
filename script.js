65e91f6e34460ba5e514083f53193b569fb1c2df
Yuto Chau <yuto5@outlook.com>
2026-05-02 22:25:23 -0400

    Refactor calculator HTML structure and add JavaScript decimal handling

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-let n1 = 0;
-let op = null;
-let n2 = null;
+let n1 = "0";
+let op = "";
+let n2 = "";
+let n1HasDecimal = false;
+let n2HasDecimal = false;
 let resultDisplayed = false;
 const clear = document.querySelector(".clear");
+const backspace = document.querySelector(".backspace");
 const digits = document.querySelectorAll(".digit");
 const operators = document.querySelectorAll(".operator");
 const display = document.querySelector(".display");
@@ -25,35 +28,74 @@ function divide(a, b) {
 
 function operate() {
   if (op === "+") {
-    n1 = add(n1, n2);
+    n1 = add(+n1, +n2).toString();
   } else if (op === "−") {
-    n1 = subtract(n1, n2);
+    n1 = subtract(+n1, +n2).toString();
   } else if (op === "×") {
-    n1 = multiply(n1, n2);
-  } else if (n1 === 0 && n2 === 0) {
+    n1 = multiply(+n1, +n2).toString();
+  } else if (n1 == 0 && n2 == 0) {
     alert("Indeterminate");
-  } else if (n2 === 0) {
+  } else if (n2 == 0) {
     alert("Undefined");
   } else {
-    n1 = divide(n1, n2);
+    n1 = divide(+n1, +n2).toString();
   }
-  op = null;
-  n2 = null;
+  op = "";
+  n2 = "";
+  n2HasDecimal = false;
+  n1HasDecimal = n1.includes(".");
 }
 
 function updateNumberVars(n) {
+  // start new calculation
   if (resultDisplayed) {
-    n1 = +n;
+    if (n === ".") {
+      n1HasDecimal = true;
+      n1 = "0.";
+    } else {
+      n1 = n;
+    }
     resultDisplayed = false;
-  } else if (op === null) {
-    n1 = +(n1 + n);
+  } else if (op === "") {
+    if (n === "." && n1HasDecimal) {
+      return;
+    }
+    if (n === "0" && n1 === "0") {
+      return;
+    }
+    if (n !== "." && n1 === "0") {
+      n1 = n;
+      return;
+    }
+    if (n === ".") {
+      n1HasDecimal = true;
+    }
+    n1 += n;
   } else {
-    n2 = n2 === null ? +n : +(n2 + n);
+    if (n === "." && n2HasDecimal) {
+      return;
+    }
+    if (n === "0" && n2 === "0") {
+      return;
+    }
+    if (n === "." && n2 === "") {
+      n2HasDecimal = true;
+      n2 = "0.";
+    } else if (n === ".") {
+      n2HasDecimal = true;
+      n2 += n;
+    } else if (n2 === "0") {
+      n2 = n;
+    } else {
+      n2 += n;
+    }
   }
 }
 
 function updateDisplay() {
-  display.textContent = `${n1}${op !== null ? ` ${op}` : ""}${n2 !== null ? ` ${n2}` : ""}`;
+  display.textContent = n1;
+  display.textContent += op !== "" ? ` ${op}` : "";
+  display.textContent += n2 !== "" ? ` ${n2}` : "";
 }
 
 for (const digit of digits) {
@@ -65,7 +107,7 @@ for (const digit of digits) {
 
 for (const operator of operators) {
   operator.addEventListener("click", () => {
-    if (n2 !== null) {
+    if (n2 !== "") {
       operate();
       resultDisplayed = true;
     }
@@ -78,8 +120,11 @@ for (const operator of operators) {
 }
 
 clear.addEventListener("click", () => {
-  n1 = 0;
-  op = null;
-  n2 = null;
+  n1 = "0";
+  op = "";
+  n2 = "";
+  n1HasDecimal = false;
+  n2HasDecimal = false;
+  resultDisplayed = false;
   updateDisplay();
 });
